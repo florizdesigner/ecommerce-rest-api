a395570db45fe7d87cc2ed1b17a6ae6d73091307
florizdesigner <vyacheslav.sokolovv@icloud.com>
2024-01-13 00:37:32 +0300

    add items, brands, types controllers (layout)

---
+++ src/users/users.controller.ts
@@ -0,0 +1,54 @@
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './users.model';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
+// import {ValidationPipe} from '../pipes/validation.pipe'; // если юзать кастомный pipes > validation.pipe.ts
+
+export interface Logger {
+  log(message: string): void;
+}
+
+@ApiTags('Users')
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 201, type: User })
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, type: [User] })
+  // @Roles('user')
+  // @UseGuards(JwtAuthGuard)
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+
+  @ApiOperation({ summary: 'Add new role to user' })
+  @ApiResponse({ status: 200 })
+  @Roles('admin')
+  @UseGuards(JwtAuthGuard)
+  @Post('/role')
+  addRole(@Body() roleDto: AddRoleDto) {
+    return this.usersService.addRole(roleDto);
+  }
+
+  @ApiOperation({ summary: 'Ban user' })
+  @ApiResponse({ status: 200 })
+  @Roles('admin')
+  @UseGuards(JwtAuthGuard)
+  @Post('/ban')
+  banUser(@Body() banDto: BanUserDto) {
+    return this.usersService.banUser(banDto);
+  }
+}
---
+++ src/users/users.module.ts
@@ -0,0 +1,22 @@
+import {forwardRef, Logger, Module} from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './users.model';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Role } from '../roles/roles.model';
+import { UserRoles } from '../roles/user-roles.model';
+import { RolesModule } from '../roles/roles.module';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [
+    Logger,
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule,
+    forwardRef(() => AuthModule)
+  ],
+  exports: [UsersService],
+})
+export class UsersModule {}
